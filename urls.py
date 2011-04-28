2af474a926bb6b54e7e1a56d84ea7d21277a0a93
rgaudin <rgaudin@gmail.com>
2011-04-28 17:04:43 +0000

    initial project setup

---
+++ urls.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding=utf-8
+# maintainer: rgaudin
+
+from django.conf.urls.defaults import patterns, include, url
+from django.contrib import admin
+
+from pnlp_web import urls as pnlp_urls
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+	url(r'', include(pnlp_urls)),
+    url(r'^admin/', include(admin.site.urls)),
+)
