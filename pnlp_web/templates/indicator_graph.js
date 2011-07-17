b0e2f9011a5087b381f221799b0e24d588d4d038
rgaudin <rgaudin@gmail.com>
2011-07-17 16:49:37 +0000

    fixed graph BG in safari

--- pnlp_web/templates/indicator_graph.js
+++ pnlp_web/templates/indicator_graph.js
@@ -1,6 +1,6 @@
 {% load babel %}{% load pnlp %}PERCENT = {% if graph.options.only_percent %}'%'{% else %}''{% endif %};
 {{ id}} = new Highcharts.Chart(
-        {chart: {renderTo: '{{ id }}', defaultSeriesType: '{{ graph.graph_type }}', backgroundColor: 'transparent', },
+        {chart: {renderTo: '{{ id }}', defaultSeriesType: '{{ graph.graph_type }}', backgroundColor: '#ebebeb', },
         legend: {}, title: {text: null},
         xAxis: {categories: [{% for p in graph.periods %}'{{ p.middle|datefmt:"MMMM YYYY" }}',{% endfor %}]},
         yAxis: {title: {text: null},{% if graph.options.only_percent %} max:100{% endif %}},
