db2e6b4e9291a14a6ef921fc5ee6eb34251e29ba
reg <rgaudin@gmail.com>
2011-11-17 09:47:42 +0000

    updated docs

---
+++ tools/export_locations.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# encoding=utf-8
+# maintainer: rgaudin
+
+import sys
+
+from bolibana_reporting.models import Entity, EntityType
+
+
+def export_locations(csv_file):
+    """ Export Entity objects into a CSV file
+
+    CSV FORMAT:
+    REGION, DISTRICT, CSCOM, CODE, TYPE """
+
+    errors = []
+
+    #def region_for(e):
+    def get_ancestors(entity, include_self=True):
+        ancestors = []
+        ee = entity if include_self else entity.parent
+        while ee.parent:
+            ancestors.append(ee)
+            ee = ee.parent
+        ancestors.append(ee)
+        ancestors.reverse()
+        return ancestors
+
+    first = True
+    f = open(csv_file, 'w')
+
+    header = u"Région,District,Unité sanitaire,Code,Niveau\n"
+    f.write(header.encode('utf-8'))
+
+    for entity in Entity.objects.all().order_by('parent'):
+
+        if not entity.parent:
+            continue
+
+        #anc = u",".join([e.display_name() for e in entity.get_ancestors(include_self=True)[1:]])
+        anc = u",".join([e.display_name() for e in get_ancestors(entity, include_self=True)[1:]])
+
+        
+
+        if entity.type.slug == 'district':
+            anc += ','
+        if entity.type.slug == 'region':
+            anc += ',,'
+        line = u"%(anc)s,%(code)s,%(type)s\n" % {'anc': anc, 'code': entity.slug, 'type': entity.type}
+
+        f.write(line.encode('utf-8'))
+        print(entity)
+    f.close()
+
+if __name__ == '__main__':
+    if sys.argv.__len__() < 2:
+        print("No CSV file specified. exiting.")
+        exit(1)
+
+    export_locations(sys.argv[1])
---
+++ tools/import_users.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# encoding=utf-8
+# maintainer: rgaudin
+
+import sys
+
+from bolibana_auth.models import Role, Provider, Access
+from bolibana_auth.utils import username_from_name, random_password
+from bolibana_reporting.models import Entity
+
+
+def import_users(csv_file):
+    """ creates Provider object off a CSV filename
+
+    CSV FORMAT:
+    FIRST NAME, LAST NAME, EMAIL, PHONE, PHONE2, ROLE CODE, ENTITY CODE
+    CSV file MUST include header row. """
+
+    errors = []
+
+    first = True
+    f = open(csv_file)
+    succ = open('success.csv', 'w')
+    for line in f.readlines():
+        if first:
+            first = False
+            continue
+        # explode CSV line
+        fname, lname, email, \
+        phone_number, phone2, role_code, entity_code, area = line.strip().split(',')
+
+        # convert name to unicode for django & .title()
+        try:
+            fname = unicode(fname, 'utf-8')
+        except:
+            pass
+
+        try:
+            lname = unicode(lname, 'utf-8')
+        except:
+            pass
+
+        # retrieve parent object if address is provided
+        try:
+            entity = Entity.objects.get(slug=entity_code.lower())
+        except:
+            print("BAD ENTITY: %s" % entity_code)
+            #raise
+            errors.append(line)
+            continue
+
+        try:
+            role = Role.objects.get(slug=role_code.lower())
+        except:
+            print("BAD ROLE: %s" % role_code)
+            #raise
+            errors.append(line)
+            continue
+
+
+        # build an Access based on Role and Entity selected
+        # if a national role, force attachment to root entity
+        if role.slug in ('antim', 'national'):
+            entity = Entity.objects.filter(level=0)[0]
+        elif role.slug != entity.type.slug:
+            print("ROLE MISMATCH LOCATION TYPE: %s / %s (%s)" % (role_code, entity_code, entity.type))
+            errors.append(line)
+            continue
+        access = Access.find_by(role, entity)
+        
+        # forge username
+        username = username_from_name(fname, lname)
+        # generate password
+        password = random_password()
+
+        pcount = Provider.objects.filter(phone_number=phone_number).count()
+
+        if phone_number and pcount > 0:
+            print("PHONE NUMBER CONFLICT: %s" % phone_number)
+            errors.append(line)
+            continue
+
+        # create Provider
+        provider = Provider.create_provider(username, \
+                                            'xx', access=[access])
+        provider.set_password(password)
+
+        # we have a valid provider whatever the case. update details
+        provider.first_name = fname
+        provider.last_name = lname
+        provider.email = email
+        # only update if not None to preserve uniqueness
+        
+        if phone_number and pcount == 0:
+            provider.phone_number = phone_number
+
+        provider.save()
+
+        line = u"%(ent)s,%(role)s,%(fname)s,%(lname)s,%(user)s,%(pass)s\n" % {'ent': entity, 'role': role, 'fname': provider.first_name, 'lname': provider.last_name, 'user': username, 'pass': password}
+        succ.write(line.encode('utf-8'))
+        print(provider.name_access())
+    f.close()
+    succ.close()
+
+    if errors:
+        error_file = 'errors.csv'
+        f = open(error_file, 'w')
+        for line in errors:
+            f.write(line)
+        f.close()
+        print("%d errors appended to file %s" % (errors.__len__(), error_file))
+
+if __name__ == '__main__':
+    if sys.argv.__len__() < 2:
+        print("No CSV file specified. exiting.")
+        exit(1)
+
+    import_users(sys.argv[1])
