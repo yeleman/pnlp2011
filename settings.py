6516d932a21cd2e5e6989ab70dc32072ff182fd0
reg <rgaudin@gmail.com>
2011-11-17 09:23:37 +0000

    added closed option

--- settings.py
+++ settings.py
@@ -222,6 +222,8 @@ ENABLE_FORTUNE = True
 
 USE_HTTPS = False
 
+SYSTEM_CLOSED = False
+
 TEMPLATE_CONTEXT_PROCESSORS = ("django.contrib.auth.context_processors.auth",
     "django.core.context_processors.debug",
     "django.core.context_processors.i18n",
--- urls.py
+++ urls.py
@@ -2,14 +2,46 @@
 # encoding=utf-8
 # maintainer: rgaudin
 
+import os
+
+from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import direct_to_template
 from django.contrib import admin
+from settings import STATIC_ROOT, MEDIA_ROOT
 
 from pnlp_web import urls as pnlp_urls
+from pnlp_web import views
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'', include(pnlp_urls)),
-    url(r'^admin/', include(admin.site.urls)),
-)
+if hasattr(settings, 'SYSTEM_CLOSED') and settings.SYSTEM_CLOSED:
+    urlpatterns = patterns('',
+        url(r'^admin/', include(admin.site.urls)),
+        url(r'', direct_to_template, {'template': 'closed.html'}, name='index'),
+
+    # development only
+    url(r'^static/admin/(?P<path>.*)$',
+             'django.views.static.serve',
+             {'document_root': os.path.join(os.path.dirname(\
+                                            os.path.abspath(admin.__file__)), \
+                               'media'), 'show_indexes': True}, \
+             name='static_admin'),
+
+    url(r'^static/(?P<path>.*)$',
+             'django.views.static.serve',
+             {'document_root': STATIC_ROOT, 'show_indexes': True}, \
+             name='static'),
+
+    url(r'^media/(?P<path>.*)$',
+             'django.views.static.serve',
+             {'document_root': MEDIA_ROOT, 'show_indexes': True}, \
+             name='media'),
+
+    )
+else:
+    urlpatterns = patterns('',
+        url(r'', include(pnlp_urls)),
+        url(r'^admin/', include(admin.site.urls)),
+    )
+
