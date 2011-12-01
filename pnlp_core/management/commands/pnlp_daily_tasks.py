fe0ce60e05b4f587f694b4249887ab190461de99
renaud gaudin <rgaudin@gmail.com>
2011-12-01 12:14:30 +0000

    safe unicode

--- pnlp_core/management/commands/pnlp_daily_tasks.py
+++ pnlp_core/management/commands/pnlp_daily_tasks.py
@@ -27,7 +27,7 @@ class Command(BaseCommand):
 
         translation.activate(settings.DEFAULT_LOCALE)
 
-        logger.info("Launching PNLP daily tasks script")
+        logger.info(u"Launching PNLP daily tasks script")
 
         now = datetime.now()
         period = current_reporting_period()
@@ -35,47 +35,47 @@ class Command(BaseCommand):
         # if new incoming report
         report = MalariaReportCreated.create(period=period, persit=False)
         if report.can_trigger():
-            logger.info("Incoming reports.")
+            logger.info(u"Incoming reports.")
             report.trigger()
 
         # if end of CSCOM period
         cscom = EndOfCSComPeriod.create(period=period)
         if cscom.can_trigger():
-            logger.info("End of CSCom reporting period.")
+            logger.info(u"End of CSCom reporting period.")
             cscom.trigger()
 
         # if end of DISTRICT period
         district = EndOfDistrictPeriod.create(period=period, is_district=True)
         if district.can_trigger():
-            logger.info("End of District reporting period.")
+            logger.info(u"End of District reporting period.")
             district.trigger()
 
         # if end of REGION period
         region = EndOfDistrictPeriod.create(period=period, is_district=False)
         if region.can_trigger():
-            logger.info("End of Region reporting period.")
+            logger.info(u"End of Region reporting period.")
             region.trigger()
 
         cscom_reminder = Reminder.create(period=period, level='cscom')
         if cscom_reminder.can_trigger():
-            logger.info("CSCom reminder.")
+            logger.info(u"CSCom reminder.")
             cscom_reminder.trigger()
 
         district_reminder = Reminder.create(period=period, level='district')
         if district_reminder.can_trigger():
-            logger.info("District reminder.")
+            logger.info(u"District reminder.")
             district_reminder.trigger()
 
         region_reminder = Reminder.create(period=period, level='region')
         if region_reminder.can_trigger():
-            logger.info("Region reminder.")
+            logger.info(u"Region reminder.")
             region_reminder.trigger()
 
         # last day of month
         #   send SMS+email to hotline so they send units to everybody
         eom = EndOfMonth.create(period=period)
         if eom.can_trigger():
-            logger.info("End of Month.")
+            logger.info(u"End of Month.")
             eom.trigger()
 
         translation.deactivate()
