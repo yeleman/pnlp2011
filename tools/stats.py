1c891145d265f6a24f9bb6dd2ad20cde80707488
renaud gaudin <rgaudin@gmail.com>
2012-01-06 08:16:05 +0000

    added per-district stats helper

---
+++ tools/stats.py
@@ -0,0 +1,23 @@
+#/usr/bin/env python
+
+from bolibana.models import *
+from pnlp_core.models import *
+
+def data(slug, period):
+    rep = MalariaReport.objects.filter(entity__parent__slug=slug, period=period).count()
+    ent = Entity.objects.get(slug=slug).get_children().count()
+    percent = float(rep) / ent
+    return (rep, ent, percent) 
+
+def stats(month, year):
+    period = Period.find_create_from(month=month, year=year)
+    for s, n in {'sego2': u"Segou",
+                 'mark': u"Markala",
+                 'nion': u"Niono",
+                 'bla': u"Bla",
+                 'tomi': u"Tominian",
+                 'baro': u"Baroueli",
+                 'san': u"San",
+                 'maci': u"Macina"}.items():
+        d = data(s, period)
+        print(u"%s\t%d\t%d\t%f" % (n, d[0], d[1], d[2]))
