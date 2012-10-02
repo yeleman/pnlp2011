903eae3f985fb115b4aa36d17cca0e44f9f5a997
Alou Dolo <alou@yeleman.com>
2012-10-02 15:38:52 +0000

    fixation de la plage de valeurs des gaphes avec le %

--- pnlp_web/templates/indicator_graph.js
+++ pnlp_web/templates/indicator_graph.js
@@ -3,7 +3,7 @@
         {chart: {renderTo: '{{ id }}', defaultSeriesType: '{{ graph.graph_type }}', backgroundColor: '#ebebeb', },
         legend: {}, title: {text: null},
         xAxis: {categories: [{% for p in graph.periods %}'{{ p.middle|datefmt:"MMMM YYYY" }}',{% endfor %}]},
-        yAxis: {title: {text: null},{% if graph.options.only_percent %} max:105{% endif %}},
+        yAxis: {title: {text: null},min:0,{% if graph.options.only_percent %} max:100{% endif %}},
         series: [{%for key, line in graph.data %}{name: '{{ line.label }}', data: [{% for pid, lvalue in line.values.items|sorted %}{% if graph.options.only_percent %}{{ lvalue.percent|default:"0"|percentraw }}{% else %}{{ lvalue.value|default_if_none:"-0" }}{% endif %},{% endfor %}]},{% endfor %}],
         tooltip: {formatter: function() { return ''+ this.series.name +': '+ this.y +PERCENT;} },
         plotOptions: {line: {animation: false, dataLabels: {enabled: true}, enableMouseTracking: false },
