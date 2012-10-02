e4f5336531546efe0e8803c30b51a05e6062b288
renaud gaudin <rgaudin@gmail.com>
2012-10-02 16:36:57 +0000

    percent graph precision

--- pnlp_web/templates/indicator_graph.js
+++ pnlp_web/templates/indicator_graph.js
@@ -4,7 +4,7 @@
         legend: {}, title: {text: null},
         xAxis: {categories: [{% for p in graph.periods %}'{{ p.middle|datefmt:"MMMM YYYY" }}',{% endfor %}]},
         yAxis: {title: {text: null},min:0,{% if graph.options.only_percent %} max:100{% endif %}},
-        series: [{%for key, line in graph.data %}{name: '{{ line.label }}', data: [{% for pid, lvalue in line.values.items|sorted %}{% if graph.options.only_percent %}{{ lvalue.percent|default_if_none:"null"|percentraw }}{% else %}{{ lvalue.value|default_if_none:"null" }}{% endif %},{% endfor %}]},{% endfor %}],
+        series: [{%for key, line in graph.data %}{name: '{{ line.label }}', data: [{% for pid, lvalue in line.values.items|sorted %}{% if graph.options.only_percent %}{{ lvalue.percent|default_if_none:"null"|percentraw:1 }}{% else %}{{ lvalue.value|default_if_none:"null" }}{% endif %},{% endfor %}]},{% endfor %}],
         tooltip: {formatter: function() { return ''+ this.series.name +': '+ this.y +PERCENT;} },
         plotOptions: {line: {animation: false, dataLabels: {enabled: true}, enableMouseTracking: false },
                       column: {animation: false, enableMouseTracking: false, dataLabels: {enabled: true, formatter: function() {if (this.y === null) { return "n/a" } else { return '' + this.y.toString().replace('.', ',') + PERCENT;} }} }},
