05d24a858609626e5915c8c7792ec773d3f4a5fd
Alou Dolo <alou@yeleman.com>
2012-10-31 16:50:46 +0000

    correction de la date des graphes

--- pnlp_web/templates/indicator_graph.js
+++ pnlp_web/templates/indicator_graph.js
@@ -2,7 +2,7 @@
 {{ id}} = new Highcharts.Chart(
         {chart: {renderTo: '{{ id }}', defaultSeriesType: '{{ graph.graph_type }}', backgroundColor: '#ebebeb', },
         legend: {}, title: {text: null},
-        xAxis: {categories: [{% for p in graph.periods %}'{{ p.middle|datefmt:"MMMM YYYY" }}',{% endfor %}]},
+        xAxis: {categories: [{% for p in graph.periods %}'{{ p.middle|graph_date_fmt:graph.periods }}',{% endfor %}]},
         yAxis: {title: {text: null},min:0,{% if graph.options.only_percent %} max:100{% endif %}},
         series: [{%for key, line in graph.data %}{name: '{{ line.label }}', data: [{% for pid, lvalue in line.values.items|sorted %}{% if graph.options.only_percent %}{{ lvalue.percent|default_if_none:"null"|percentraw:1 }}{% else %}{{ lvalue.value|default_if_none:"null" }}{% endif %},{% endfor %}]},{% endfor %}],
         tooltip: {formatter: function() { return ''+ this.series.name +': '+ this.y +PERCENT;} },
