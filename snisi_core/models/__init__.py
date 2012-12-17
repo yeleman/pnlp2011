c94ad9290e73da6d8fd7229a72592cdd8dc4787d
Alou Dolo <alou@yeleman.com>
2012-12-17 12:05:56 +0000

    maj du init du models

--- snisi_core/models/__init__.py
+++ snisi_core/models/__init__.py
@@ -7,8 +7,5 @@ from alert import Alert
 from MaternalMortalityReport import MaternalMortalityReport
 from ChildrenMortalityReport import ChildrenMortalityReport
 from CommoditiesReport import RHCommoditiesReport
-from ProvidedServices import ProvidedServicesReport
-from BirthReport import BirthReport
-from PregnancyReport import PregnancyReport
 from Epidemiology import EpidemiologyReport
 
